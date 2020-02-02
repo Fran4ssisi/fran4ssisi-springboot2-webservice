f9844247b2f8a5600080e2ac62e907f60ba2fa62
Fran4ssisi <carloes87@gmail.com>
2020-02-02 21:03:54 +0900

    lombok 설정

---
+++ src/main/java/com/fran4ssisi/boot/spring/Application.java
@@ -0,0 +1,11 @@
+package com.fran4ssisi.boot.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
