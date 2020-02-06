e73d44bc3c8aa99f0a2dd8dad33f7c209416fff6
Fran4ssisi <carloes87@gmail.com>
2020-02-06 21:38:57 +0900

    등록 api 수정

--- src/main/java/com/fran4ssisi/boot/spring/web/PostsApiController.java
+++ src/main/java/com/fran4ssisi/boot/spring/web/PostsApiController.java
@@ -3,6 +3,7 @@ package com.fran4ssisi.boot.spring.web;
 import com.fran4ssisi.boot.spring.service.Postsservice;
 import com.fran4ssisi.boot.spring.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ public class PostsApiController {
     //@Autowired를 하지 않는 이유는 생성자로 Bean을 주입받고 있기때문(롬복)
     private final Postsservice postsService;
 
-    @PutMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
