8b7ee5f7a7f2d7bd0e96c6c8921122e44708d64b
Fran4ssisi <carloes87@gmail.com>
2020-02-18 07:15:50 +0900

    스프링시큐리티와 QAuth 2.0 로그인기능 구현

--- src/main/java/com/fran4ssisi/boot/spring/web/IndexController.java
+++ src/main/java/com/fran4ssisi/boot/spring/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.fran4ssisi.boot.spring.web;
 
+import com.fran4ssisi.boot.spring.config.auth.dto.SessionUser;
+import com.fran4ssisi.boot.spring.domain.user.User;
 import com.fran4ssisi.boot.spring.service.PostsService;
 import com.fran4ssisi.boot.spring.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        //앞서 작성된 CustomOauth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
